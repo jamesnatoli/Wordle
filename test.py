0ed3d94a7961abf0602ec77e3ec25b835f0ffc53
James Natoli <james.v.natoli@gmail.com>
2022-09-27 00:00:48 +0200

    driver incorporates objects

--- test.py
+++ test.py
@@ -2,10 +2,12 @@
 # James Natoli, 2022
 
 from optparse import OptionParser
+from termcolor import colored, cprint
 
 # Local Classes
-from Game import Game
+# from Game import Game
 from Player import Player
+from Letter import Letter
 
 parser = OptionParser()
 parser.add_option('--player', '-p', action='store_true', dest='player', 
@@ -24,8 +26,49 @@ if ops.__dict__['compete'] is not None and (ops.__dict__['player'] is not None o
     parser.error('Please only choose one option')
 
 def main():
-    driver = Game()
+    # put Game object in Player() ?
+    # driver = Game()
     passenger = Player()
+
+    # print( colored(' W ', 'white', 'on_grey') + colored(' O ', 'white', 'on_yellow') + colored(' R ', 'white', 'on_green') +
+    # colored(' D ', 'white', 'on_grey') + colored(' L ', 'white', 'on_yellow') + colored(' E ', 'white', 'on_green'))
+
+    guess = []
+    print('Welcome to ' + colored(' W ', 'white', 'on_grey') + colored(' O ', 'white', 'on_yellow') + colored(' R ', 'white', 'on_green') +
+           colored(' D ', 'white', 'on_grey') + colored(' L ', 'white', 'on_yellow') + colored(' E ', 'white', 'on_green'))
+
+    # guess will be sent to our Player object
+    # Player will store the array of Letters as a member
+    # do the work inside of Player...
+    passenger.runGame()
+
+
+    exit(0)
+    # create an array of Letter objects, which contain color info
+    for i in range( 0, len(inguess)):
+        guess.append( Letter( inguess[i], i))
+    
+    # Main Loop...
+    # feedback = driver.nextGuess( guess)
+    for i in feedback:
+        cprint( i.getText(), i.getColor())
+    
+    for i in range(0, 4):
+        guess = []
+        inguess = input("Guess %d/6: \t"%(i+2))
+        assert len( inguess) == 5, 'WORDLE NOT HAPPY'
+        
+        for i in range( 0, len(inguess)):
+            guess.append( letter( inguess[i], i))
+            
+        checkedGuess = check( guess, answer)
+        
+        for i in checkedGuess:
+            cprint( i.getText(), i.getColor())
+
+        if inguess == answer:
+            print('You\'ve got it!')
+            break
 
 if __name__ == "__main__":
     main()
